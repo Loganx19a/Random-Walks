3afbfe3929c5b8ea7a5132b9c61edbd3dcf3fec0
Logan <Loganx19a@ksu.edu>
2024-01-01 19:18:00 -0600

    incr points to 50,000, adjusted the size of Matplotlib's output to better fit screen, and added dark_background

--- rw_visual.py
+++ rw_visual.py
@@ -4,15 +4,15 @@ from random_walk import RandomWalk
 
 while True:
     # Make a random walk
-    rw = RandomWalk()
+    rw = RandomWalk(50_000)
     rw.fill_walk()
 
     # Plot the points in the walk
-    plt.style.use('classic')
-    fig, ax = plt.subplots()
+    plt.style.use('dark_background')
+    fig, ax = plt.subplots(figsize=(10, 8), dpi=137)               # adjust the size of Matplotlib's output to better fit screen
     point_numbers = range(rw.num_points)
     ax.scatter(rw.x_values, rw.y_values, c=point_numbers, # to visualize the walk, we feed the walk's x and y-values to scatter()
-        cmap=plt.cm.Blues, edgecolors='none', s=15)      
+        cmap=plt.cm.Blues, edgecolors='none', s=2)      
     ax.set_aspect('equal')                               # specify that both axes should have equal spacing between tick marks
     
     # emphasize the first and last points
